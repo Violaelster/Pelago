0678b75fa6ab889c93978e42f4f618c24dcd0b27
Violaelster <viola.svea@gmail.com>
2024-12-15 00:24:40 +0100

    Adding sections

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<?php include __DIR__ . '/../components/header.html'; ?>
+<main>
+    <section id="hero">
+        <img src="/../assets/images/knuckles.jpg" alt="Description of the image" id="hero-index">
+
+
+
+        <section id="welcoming">
+            <p>Welcome to Smooth Motel, where every stay is smoother than a Snoop Dogg beat. Immerse yourself in a world of laid-back luxury and unforgettable experiences. Whether you're here to chill in our G-funk-inspired suites, sip on a signature cocktail at the Gin & Juice Lounge, or simply enjoy the pink paradise we've created, Smooth Motel is your ultimate escape.</p>
+
+            <p>It's not just a place to stay—it's a lifestyle, handpicked by the Doggfather himself. Ready to live smooth? Book your stay now and join the vibe.</p>
+        </section>
+
+        <article id="spot">
+            <p>
+                Get yo spot
+                <button class="spot-button">here</button>
+            </p>
+        </article>
+
+        <section id="shortcuts">
+            <div class="svg-container">
+                <img src="/assets/svg/marthas-kitchen.svg" alt="Martha's Kitchen" class="svg-item">
+            </div>
+            <div class="svg-container">
+                <img src="/assets/svg/gin-and-juice.svg" alt="Gin & Juice Lounge" class="svg-item">
+            </div>
+            <div class="svg-container">
+                <img src="/assets/svg/g-funk-nights.svg" alt="G-funk nights" class="svg-item">
+
+            </div>
+            <div class="svg-container">
+                <img src="/assets/svg/the-bling-shop.svg" alt="Gin & Juice Lounge" class="svg-item">
+
+            </div>
+        </section>
+
+
+
+    </section>
+</main>
+</body>
